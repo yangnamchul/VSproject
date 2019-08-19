4784a2b8a7e1a873a3ce46b2313ed61274d000ef
KGITBANK <KGITBANK@DESKTOP-S1PLQAG>
2019-08-19 21:15:38 +0900

    controller

--- VSproject/src/main/java/com/vs/my/HomeController.java
+++ VSproject/src/main/java/com/vs/my/HomeController.java
@@ -1,15 +1,11 @@
 package com.vs.my;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -17,23 +13,114 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping(value = "/", method = RequestMethod.GET) //메인페이지
+	public ModelAndView Main(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Main");
+		
+		return mv;
+	}
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	///////////////////////////// 회원 관련 ///////////////////////////////////
+	
+	@RequestMapping(value="/SignUp", method=RequestMethod.GET) //회원가입
+	public ModelAndView SignUp(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("SingUp");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		return mv;
+	}
+	
+	@RequestMapping(value="/Terms", method=RequestMethod.GET) //약관 동의
+	public ModelAndView Terms(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Terms");
 		
-		String formattedDate = dateFormat.format(date);
+		return mv;
+	}
+	
+	@RequestMapping(value="/Login", method=RequestMethod.POST) //로그인
+	public ModelAndView Login(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Login");
 		
-		model.addAttribute("serverTime", formattedDate );
+		return mv;
+	}
+	
+	@RequestMapping(value="/FindID", method=RequestMethod.POST) //아이디 찾기
+	public ModelAndView FindID(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("FindID");
 		
-		return "home";
+		return mv;
+	}
+	
+	@RequestMapping(value="/FindPW", method=RequestMethod.POST) //비밀번호 찾기
+	public ModelAndView FindPW(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("FindPW");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/MyPage", method=RequestMethod.GET) //마이페이지
+	public ModelAndView MyPage(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("MyPage");
+		
+		return mv;
+	}
+	
+	//////////////////////////// 게시판 관련 /////////////////////////////////
+	
+	@RequestMapping(value="/Board", method=RequestMethod.GET) //게시판
+	public ModelAndView Board(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Board");
+		
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/Category",method=RequestMethod.GET) // 카테고리
+	public ModelAndView Category(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Category");
+		
+		return mv;
+	}
+	
+	
+	@RequestMapping(value="/OneView", method=RequestMethod.GET) //글 하나 보기
+	public ModelAndView OneView(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("OneView");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/WritePost", method=RequestMethod.POST) //글 작성 화면
+	public ModelAndView WritePost(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("WritePost");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/EditPost", method=RequestMethod.POST) //글 수정 화면
+	public ModelAndView EditPost(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("EditPost");
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/Search", method=RequestMethod.POST) //검색 결과
+	public ModelAndView Search(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Search");
+		
+		return mv;
 	}
 	
 }
