feaf6d04e51277593550be9ab89ee57db7cbd340
GuRem <lgurem@naver.com>
2019-08-20 20:58:46 +0900

    8:58 개인 메인수정

--- VSproject/src/main/java/com/vs/my/HomeController.java
+++ VSproject/src/main/java/com/vs/my/HomeController.java
@@ -17,128 +17,126 @@ import com.vs.my.UserDAOVO.UserVO;
  */
 @Controller
 public class HomeController {
-	
+
 	UserDAO ud = new UserDAO();
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET) //메인페이지
+
+	@RequestMapping(value = "/", method = RequestMethod.GET) // 메인페이지
 	public ModelAndView Main(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Main");
-		
+
 		return mv;
 	}
-	
+
 	///////////////////////////// 회원 관련 ///////////////////////////////////
-	
-	@RequestMapping(value="/SignUp", method=RequestMethod.GET) //회원가입
+
+	@RequestMapping(value = "/SignUp", method = RequestMethod.GET) // 회원가입
 	public ModelAndView SignUp(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("SingUp");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/Terms", method=RequestMethod.GET) //약관 동의
+
+	@RequestMapping(value = "/Terms", method = RequestMethod.GET) // 약관 동의
 	public ModelAndView Terms(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Terms");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/Login", method=RequestMethod.POST) //로그인
+
+	@RequestMapping(value = "/Login", method = RequestMethod.POST) // 로그인
 	public ModelAndView Login(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Login");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/FindID", method=RequestMethod.POST) //아이디 찾기
+
+	@RequestMapping(value = "/FindID", method = RequestMethod.POST) // 아이디 찾기
 	public ModelAndView FindID(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("FindID");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/FindPW", method=RequestMethod.POST) //비밀번호 찾기
+
+	@RequestMapping(value = "/FindPW", method = RequestMethod.POST) // 비밀번호 찾기
 	public ModelAndView FindPW(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("FindPW");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/MyPage", method=RequestMethod.GET) //마이페이지
+
+	@RequestMapping(value = "/MyPage", method = RequestMethod.GET) // 마이페이지
 	public ModelAndView MyPage(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("MyPage");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/UserAllData", method=RequestMethod.GET) //유저정보 전부보기
+
+	@RequestMapping(value = "/UserAllData", method = RequestMethod.GET) // 유저정보 전부보기
 	public ModelAndView UserAllData(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("UserAllData");
-		
+
 		List<UserVO> userlist = ud.UserAllData();
-		
+
 		mv.addObject("userlist", userlist);
-		
+
 		return mv;
 	}
 	//////////////////////////// 게시판 관련 /////////////////////////////////
-	
-	@RequestMapping(value="/Board", method=RequestMethod.GET) //게시판
+
+	@RequestMapping(value = "/Board", method = RequestMethod.GET) // 게시판
 	public ModelAndView Board(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Board");
-		
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/Category",method=RequestMethod.GET) // 카테고리
+
+	@RequestMapping(value = "/Category", method = RequestMethod.GET) // 카테고리
 	public ModelAndView Category(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Category");
-		
+
 		return mv;
 	}
-	
-	
-	@RequestMapping(value="/OneView", method=RequestMethod.GET) //글 하나 보기
+
+	@RequestMapping(value = "/OneView", method = RequestMethod.GET) // 글 하나 보기
 	public ModelAndView OneView(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("OneView");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/WritePost", method=RequestMethod.POST) //글 작성 화면
+
+	@RequestMapping(value = "/WritePost", method = RequestMethod.POST) // 글 작성 화면
 	public ModelAndView WritePost(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("WritePost");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/EditPost", method=RequestMethod.POST) //글 수정 화면
+
+	@RequestMapping(value = "/EditPost", method = RequestMethod.POST) // 글 수정 화면
 	public ModelAndView EditPost(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("EditPost");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value="/Search", method=RequestMethod.POST) //검색 결과
+
+	@RequestMapping(value = "/Search", method = RequestMethod.POST) // 검색 결과
 	public ModelAndView Search(HttpServletRequest req) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("Search");
-		
+
 		return mv;
 	}
-	
+
 }
--- VSproject/src/main/webapp/WEB-INF/views/Main.jsp
+++ VSproject/src/main/webapp/WEB-INF/views/Main.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +7,22 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1> 메인페이지 </h1>
-	<a href="Board"> 게시판 </a>
-	<a href="UserAllData">유저정보 전부보기</a>
+	<h1> 메인페이지</h1>
+	<div align="right"> <h2>검색 MY(로그인)</h2></div>
+
+	<hr />
+
+	<div align="center" style="color:red">
+	<h2>
+
+	<FONT COLOR="red"><a href="Board"> 게시판 </a> </FONT>
+
+	<FONT COLOR="red"> 카테고리 </FONT>
+	<FONT COLOR="red"> 최신글 </FONT>
+	<FONT COLOR="red"> 공지사항 </FONT>
+	</h2>
+	</div>
+
+
 </body>
 </html>
